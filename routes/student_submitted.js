24fb527e5d9447f6fc7e2757a2ec0730ff31f88b
robotic7o7 <scientificrohan@gmail.com>
2021-07-03 14:19:56 +0530

    latest commit

--- routes/student_submitted.js
+++ routes/student_submitted.js
@@ -34,4 +34,16 @@ router.get('/student/:id', async function (req, res, next) {
     }
 });
 
+
+//submissions by assignment ID
+
+router.get('/assignment/:id', async function (req, res, next) {
+    try{
+        const submissions= await StudentSubmissions.find({ "assignment_id": { "$regex": req.query.q, "$options": "i" }, "assignment_id":req.params.id})
+        res.status(200).json(submissions);
+    } catch (err) {
+        res.status(500).json({ error: err });
+    }
+});
+
 module.exports = router;
